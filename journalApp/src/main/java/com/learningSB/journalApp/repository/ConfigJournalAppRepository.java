e591f15dcad89feb10b57511112bc8daaea3bd98
arshbhushan <arshbhushan@gmail.com>
2024-12-28 18:13:46 +0530

    Added Config files.

---
+++ journalApp/src/main/java/com/learningSB/journalApp/repository/ConfigJournalAppRepository.java
@@ -0,0 +1,9 @@
+package com.learningSB.journalApp.repository;
+
+import com.learningSB.journalApp.entity.ConfigJournalAppEntity;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ConfigJournalAppRepository extends MongoRepository<ConfigJournalAppEntity, ObjectId> {
+
+}
