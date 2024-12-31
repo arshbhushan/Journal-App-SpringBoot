15dd444ca610af7b6f64a21e179604a5e7c1611d
arshbhushan <arshbhushan@gmail.com>
2024-12-31 14:27:58 +0530

    Initialized replacing basic auth with JTW.

--- journalApp/src/main/java/com/learningSB/journalApp/controller/PublicController.java
+++ journalApp/src/main/java/com/learningSB/journalApp/controller/PublicController.java
@@ -2,23 +2,57 @@ package com.learningSB.journalApp.controller;
 
 import com.learningSB.journalApp.entity.User;
 import com.learningSB.journalApp.service.UserService;
+import com.learningSB.journalApp.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class PublicController {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @GetMapping("/health-check")
     public String healthCheck() {
         return "OK";
     }
 
-    @PostMapping("/create-user")
-    public void createUser(@RequestBody User user) {
+    @PostMapping("/signup")
+    public void signup(@RequestBody User user) {
         userService.saveNewEntry(user);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            log.info("username : " + user.getUserName());
+            log.info("Password : " + user.getPassword());
+            String jwt= jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        }catch(Exception e){
+            log.error("Exception occurred while CreteAuthenticationToken",e);
+            return new ResponseEntity<>("Incorrect username or password",HttpStatus.BAD_REQUEST);
+        }
     }
 }
